b019f5831c6a269075df56a80791b15d4c0f2185
Fernando Perez Gomez <fernando.perez@wisphub.net>
2022-01-31 18:12:18 -0500

    Autentificacion por OAuth

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -16,18 +16,4 @@ urlpatterns = [
     url(r'^mascota/$', views.MascotaList.as_view()),
     url(r'^mascota/(?P<pk>\d+)/$', views.MascotaDetail.as_view()),
     # endregion
-
-
-    # region protected resources - persona
-    url(r'^priv/persona/$', views.PersonaPrivateList.as_view()),
-    url(r'^priv/persona/(?P<pk>\d+)/$', views.PersonaPrivateDetail.as_view()),
-    # endregion
-    # region endpoints vacuna
-    url(r'^priv/vacuna/$', views.VacunaPrivateList.as_view()),
-    url(r'^priv/vacuna/(?P<pk>\d+)/$', views.VacunaPrivateDetail.as_view()),
-    # enregion
-    # region endpoints mascota
-    url(r'^priv/mascota/$', views.MascotaPrivateList.as_view()),
-    url(r'^priv/mascota/(?P<pk>\d+)/$', views.MascotaPrivateDetail.as_view()),
-    # endregion
 ]
