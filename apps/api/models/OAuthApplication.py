0d1003411aede5260d9e02d805972c89191eebe3
Fernando Perez Gomez <fernando.perez@wisphub.net>
2022-02-16 12:22:06 -0500

    Se permiten todos los grant types

--- apps/api/models/OAuthApplication.py
+++ apps/api/models/OAuthApplication.py
@@ -3,7 +3,15 @@ from oauth2_provider.models import AbstractApplication
 
 class OAuthApplication(AbstractApplication):
     def allows_grant_type(self, *grant_types):
+        """
+        Sobreescribimos el metodo y permitimos todos los flujos de OAuth2.0.
+        Con este cambio, las aplicaciones que se registren no importara si se especifica un grant type en especifico
+        :param grant_types:
+        :return:
+        """
         return bool({self.authorization_grant_type,
                      self.GRANT_CLIENT_CREDENTIALS,
-                     self.GRANT_AUTHORIZATION_CODE
+                     self.GRANT_AUTHORIZATION_CODE,
+                     self.GRANT_PASSWORD,
+                     self.GRANT_IMPLICIT,
                      } & set(grant_types))
